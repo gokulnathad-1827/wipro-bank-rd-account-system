806860a68e79b024047720abf2402e6e2d893b7f
gokulnath-1827 <gokulnath.ad.1827@amail.com>
2026-06-27 14:42:22 +0530

    Initial commit - Wipro Bank RD Account Project

---
+++ src/com/wipro/bank/acc/RDAccount.java
@@ -0,0 +1,26 @@
+package com.wipro.bank.acc;
+
+public class RDAccount extends Account {
+
+    public RDAccount(int tenure, float principal) {
+        this.tenure = tenure;
+        this.principal = principal;
+    }
+
+    public float calculateAmountDeposited() {
+        return principal * tenure * 12;
+    }
+
+    @Override
+    public float calculateInterest() {
+
+        float p = calculateAmountDeposited();
+        float r = rateOfInterest / 100;
+        float n = 4;
+        float t = tenure;
+
+        float interest = (float) (p * (Math.pow((1 + r / n), (n * t)) - 1));
+
+        return interest;
+    }
+}
